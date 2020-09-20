99219a615d55b4698b18e56b00aba5a1d7d18fec
Sergei Alexeev <snalexeev@outlook.com>
2020-09-20 19:53:48 +0300

    save for netlify

--- public/app.js
+++ public/app.js
@@ -1,48 +1,9 @@
 console.log('i am start!!!')
 
-
-import NavBar from './elements/navbar.html'
-
 const application = document.getElementById('app')
 
-// const config = {
-//     menu: {
-//         href: '/menu',
-//         text: 'Главная',
-//         open: mainPage,
-//     },
-// signup: {
-//     href: '/signup',
-//     text: 'Зарегистрироваться',
-//     open: signupPage,
-// },
-// login: {
-//     href: '/login',
-//     text: 'Авторизоваться',
-//     open: loginPage,
-// },
-// profile: {
-//     href: '/profile',
-//     text: 'Профиль',
-//     open: profilePage,
-// },
-// about: {
-//     href: '/about',
-//     text: 'Контакты',
-// },
-// };
-
-
-function createNavBar() {
-    const nav = document.createElement()
-
-}
-
 function mainPage() {
-    // application.insertAdjacentHTML('afterend', )
-
-
-    application.innerHTML = `
+    application.innerHTML += `
         <ul>
             <li><a class="activeGreenButton" href="/">HotelScanner</a></li>
             <li><a href="/top">Лучшие отели</a></li>
