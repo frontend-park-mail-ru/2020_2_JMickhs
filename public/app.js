bb50ee4047ad60846d4162aed5bf752ef05a94fc
Sergei Alexeev <snalexeev@outlook.com>
2020-09-20 20:58:46 +0300

    удаляем все, что бы сделать всё заново

--- public/app.js
+++ public/app.js
@@ -3,22 +3,8 @@ console.log('i am start!!!')
 const application = document.getElementById('app')
 
 function mainPage() {
-    application.innerHTML += `
-        <ul>
-            <li><a class="activeGreenButton" href="/">HotelScanner</a></li>
-            <li><a href="/top">Лучшие отели</a></li>
-            <li><a href="/list">Список отелей</a></li>
-            <li class="dropdown" style="float:right"><a href="#about">Регистрация/Авторизация</a>
-                <div class="dropdown-content">
-                    <a href="/signin">Войти</a>
-                    <a href="/signup">Зарегистрироваться</a>
-                </div>
-            </li>
-        </ul>
-        `
-
-    // const menuItem = document.createElement('a')
-    // menuItem.textContent = 'ahahahh'
+    const menuItem = document.createElement('a')
+    menuItem.textContent = 'ahahahh'
 
     application.appendChild(menuItem)
 }
