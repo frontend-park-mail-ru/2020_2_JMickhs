6616686c0ed682e2e9c1f4ae6dbea222c8a508b7
Sergei Alexeev <snalexeev@outlook.com>
2020-09-27 19:31:45 +0300

    save

--- public/js/signin/signin.js
+++ public/js/signin/signin.js
@@ -4,7 +4,6 @@ class SigninController {
         this.view = view;
         this.model = model;
         this.view.subscribe('signin', (arg) => {
-            console.log('вызовем signin у модели', arg);
             model.signin(arg.email, arg.password);
         });
     }
@@ -18,10 +17,14 @@ class SignInView extends EventEmitter {
         super();
         this.model = model;
         this.app = document.getElementById('app');
+        this.model.subscribe('response', (arg) => {
+            console.log(arg.status);
+            console.log(arg.response);
+        });
     }
     show() {
         let navbar = new Navbar();
-        this.app.innerHTML = navbar.render('home', 'list', 'signin', 2) + `
+        this.app.innerHTML = navbar.render() + `
         
         <div class="container"></div>
         <form action="" class="ui-form" id="signinform">
@@ -33,7 +36,7 @@ class SignInView extends EventEmitter {
                 <input type="password" id="password"><label for="password">Пароль</label>
             </div>
             <span class="psw">Нету аккаунта? 
-                <a href="/signup.html">Регистрация</a>
+                <a href="#/signup">Регистрация</a>
             </span>
             <button class="btn" type="submit" id="btnsignin">Вход</button>
         </form>
@@ -60,14 +63,22 @@ class UserModel extends EventEmitter {
         this.id = -1;
         this.login = '';
     }
+    test(status, response) {
+        console.log('status', status);
+        console.log('response', response);
+    }
     signin(username, password) {
         ajax(
             'POST',
             'http://89.208.197.127:8080/api/v1/signin', { username, password },
-            (status, response) => {
-                console.log(status);
-                console.log(response);
-            }
+            this.test.bind(this)
+        )
+    }
+    signup(username, password) {
+        ajax(
+            'POST',
+            'http://89.208.197.127:8080/api/v1/signup', { username, password },
+            this.test.bind(this)
         )
     }
 }
