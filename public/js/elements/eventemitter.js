6451341063f5b4c8834b70a596690787bb36801c
Sergei Alexeev <snalexeev@outlook.com>
2020-09-27 15:32:13 +0300

    Create eventemitter.js

---
+++ public/js/elements/eventemitter.js
@@ -0,0 +1,19 @@
+// наследоваться от этого класса в view и model
+
+/**
+ * класс эмитирущий евенты
+ */
+class EventEmitter {
+    constructor() {
+            this.events = {}; // храним евенты
+        }
+        //добавляем евенты
+    subscribe(evt, listener) {
+            (this.events[evt] || (this.events[evt] = [])).push(listener);
+            return this;
+        }
+        //ввыполняем 
+    do(evt, arg) {
+        (this._events[evt] || []).slice().forEach(lsn => lsn(arg));
+    }
+}
