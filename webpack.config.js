10114aea71ba46f04263175c0420fcc8c9da9071
Sergei Alexeev <snalexeev@outlook.com>
2020-10-03 23:49:15 +0300

    fix html webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,21 @@
 const path = require('path');
-const webpack = require("webpack");
+//const webpack = require("webpack");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { Template } = require('webpack');
-
 
 module.exports = {
     mode: 'development',
-    entry: './public/js/app.js',    
+    entry: './public/js/app.js',
     output: {
         path: path.resolve(__dirname, './public'),
         filename: 'bundle.js'
     },
     plugins: [
         new HtmlWebpackPlugin({
-          title: 'HostelScan',
-          filename: './index.html',
-          template: './public/template.html',
-          entryPoint: 'app',
-          css: "styles.css"
+            title: 'HostelScan',
+            filename: './index.html',
+            template: './public/template.html',
+            entryPoint: 'app',
+            css: "styles.css"
         })
     ]
 };
