cd26dea9cf2295e8139b8995a80f4bc83cfb98e2
Alkirys <nik_manz@mail.ru>
2020-10-03 23:39:36 +0300

    немного подправим код Сережи)

--- public/js/components/profile/usermodel.js
+++ public/js/components/profile/usermodel.js
@@ -13,7 +13,7 @@ export default class UserModel {
         response.then((response) => {
             let status = response.status;
             let body = response.body; // TODO:
-            if (status == 200) {
+            if (status === 200) {
                 this.isAuth = true;
                 this.login = body.username;
                 EventBus.trigger('updateUser');
@@ -23,7 +23,7 @@ export default class UserModel {
     signin(username, password) {
         let response = Net.signin(username, password);
         response.then((status) => {
-            if (status == 200) {
+            if (status === 200) {
                 this.isAuth = true;
                 this.login = username;
             }
@@ -34,13 +34,10 @@ export default class UserModel {
     signup(username, password) {
         let response = Net.signup(username, password);
         response.then((status) => {
-            if (status != 200) {
-                EventBus.trigger('updateUser');
-                EventBus.trigger('signupUser');
-                return;
+            if (status === 200) {
+                this.isAuth = true;
+                this.login = username;
             }
-            this.isAuth = true;
-            this.login = username;
             EventBus.trigger('updateUser');
             EventBus.trigger('signupUser');
         });
