ec52f9eb13593804937c6cd5db443d86f93e24c6
Nikita Shchelkanov <nik.shchelkanov@gmail.com>
2020-10-04 17:12:17 +0300

    validation for signin page

--- public/js/components/signin/signinController.js
+++ public/js/components/signin/signinController.js
@@ -8,21 +8,7 @@ export default class SigninController {
         this._view = new SigninView(parent, this._model);
 
         EventBus.subscribe('submitSignin', (arg) => {
-            let { login, password } = arg;
-            let canSend = true;
-            if (login === '') {
-                this._view.renderErrLogin(true, 'Введите логин!');
-                canSend = false;
-            } else {
-                this._view.renderErrLogin(false);
-            }
-            if (password === '') {
-                this._view.renderErrPassword(true, 'Введите пароль!');
-                canSend = false;
-            } else {
-                this._view.renderErrPassword(false);
-            }
-
+            let canSend = this.validate(arg)
             if (canSend) {
                 this._model.signin(arg.login, arg.password);
             }
@@ -34,5 +20,32 @@ export default class SigninController {
             return;
         }
         this._view.render();
+    }
+
+    validate(arg) {
+        let { login, password } = arg;
+        let canSend = true;
+        if (login === '') {
+            this._view.renderErrLogin(true, 'Введите логин!');
+            canSend = false;
+        } else if (login.length > 20) {
+            this._view.renderErrLogin(true, 'Логин не может быть больше 20 символов');
+            canSend = false;
+        } else {
+            this._view.renderErrLogin(false);
+        }
+        if (password === '') {
+            this._view.renderErrPassword(true, 'Введите пароль!');
+            canSend = false;
+        } else if (password.length < 8) {
+            this._view.renderErrPassword(true, 'Пароль не может быть меньше 8 символов');
+            canSend = false;
+        } else if (password.length > 20) {
+            this._view.renderErrPassword(true, 'Пароль не может быть больше 20 символов');
+            canSend = false;
+        } else {
+            this._view.renderErrPassword(false);
+        }
+        return canSend;
     }
 }
