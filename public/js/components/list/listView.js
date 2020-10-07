9c5ef95bba0fb4dc8dadb033b26be09469582a8a
Nikita Shchelkanov <nik.shchelkanov@gmail.com>
2020-10-07 11:36:35 +0300

    templates in progress

--- public/js/components/list/listView.js
+++ public/js/components/list/listView.js
@@ -2,6 +2,8 @@ import ListModel from './listModel';
 import Net from '../../helpers/network/networking';
 import Events from './../../helpers/eventbus/eventbus';
 
+var myTemplate = require('./listTemplate.hbs');
+
 /** Класс представления для страницы списка отелей */
 export default class ListView {
     /**
@@ -27,27 +29,39 @@ export default class ListView {
             this.render();
         });
     }
+    // render() {
+    //     let strRes = '';
+
+    //     this._model.hostels.forEach((hostel) => {
+    //         const id = hostel.id;
+    //         const urlImage = Net.getUrlFile(hostel.image);
+    //         const tmp = `
+    //         <div class="card">
+    //              <img class="avatar" src="${urlImage}" alt="Avatar">
+    //              <h3>
+    //                 <p class="hotel-card-title" href="/hostel/${id}">${hostel.name}
+    //                 </p>
+    //              </h3>
+    //               <p class="hotel-card-block">
+    //                 <a class="btn-green" href="/hostel/${id}">Подробнее</a>
+    //               </p>
+    //          </div>
+    //      `;
+    //         strRes += tmp;
+    //     });
+    //     this.page.innerHTML = strRes;
+    // }
+
     /**
      * Отрисовка страницы списка отелей
      */
     render() {
-        let strRes = '';
-        this._model.hostels.forEach((hostel) => {
-            const id = hostel.id;
-            const urlImage = Net.getUrlFile(hostel.image);
-            const tmp = `
-            <div class="card">
-                 <img class="avatar" src="${urlImage}" alt="Avatar">
-                 <h3>
-                 <p class="hotel-card-title">${hostel.name}</p>
-                 </h3>
-                  <p class="hotel-card-block">
-                    <a class="btn-green" href="/hostel/${id}">Подробнее</a>
-                  </p>      
-             </div>
-         `;
-            strRes += tmp;
-        });
-        this.page.innerHTML = strRes;
+        // console.log("fwefwefwefwefef")
+        // console.log(this._model.hostels);
+        // this.page.innerHTML.appendChild
+        console.log("hotels : ")
+        console.log(this._model.hostels)
+        const urlImg = Net.getUrlFile(this._model.image);
+        this.page.innerHTML = myTemplate(this._model.hostels);
     }
 }
