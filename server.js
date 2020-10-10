761e0154dd862be0a6135a33b6fe3dbb1d0b0446
Sergei Alexeev <snalexeev@outlook.com>
2020-10-10 23:02:27 +0300

    test precommit

--- server.js
+++ server.js
@@ -9,11 +9,11 @@ app.use(express.static(`${__dirname}/public`));
 const port = 80;
 
 app.get('*', (req, res) => {
-  console.log(req.url);
-  // eslint-disable-next-line no-undef
-  res.sendFile(`${__dirname}/public/index.html`);
+    console.log(req.url);
+    // eslint-disable-next-line no-undef
+    res.sendFile(`${__dirname}/public/index.html`);
 });
 
 app.listen(port, () => {
-  console.log(`Server listening port ${port}`);
+    console.log(`Server listening port ${port}`);
 });
