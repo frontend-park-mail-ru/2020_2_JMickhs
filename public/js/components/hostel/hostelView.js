4a212690b8ec5632a29d700bd60213a699ab621e
Sergei Alexeev <snalexeev@outlook.com>
2020-10-11 00:42:37 +0300

    удаление комментариев

--- public/js/components/hostel/hostelView.js
+++ public/js/components/hostel/hostelView.js
@@ -31,7 +31,6 @@ export default class HostelView {
      * Отрисовка страницы отеля
      */
     render() {
-        // const urlImg = Net.getUrlFile(this._model.image);
         this.page.innerHTML = hostelTemplate(this._model);
     }
 }
