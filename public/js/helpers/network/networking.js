2f1c2464de580c2d71720d04e35d9002495a2e30
Sergei Alexeev <snalexeev@outlook.com>
2020-10-14 10:34:58 +0300

    fix?

--- public/js/helpers/network/networking.js
+++ public/js/helpers/network/networking.js
@@ -28,7 +28,7 @@ class Network {
      * @return {Promise}
      */
     _ajax(method, url, body = null, headers = {}) {
-        let reqBody;
+        let reqBody = body;
         if (body != null && !(body instanceof FormData)) {
             reqBody = JSON.stringify(body); // TODO: сделать проверку
         }
