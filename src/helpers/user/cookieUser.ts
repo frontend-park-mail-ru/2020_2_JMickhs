ec4a6bf6c0e44a18bc8312f80ed5c53a746efa81
Alkirys <nik_manz@mail.ru>
2020-10-23 11:28:44 +0000

    save

--- src/helpers/user/cookieUser.ts
+++ src/helpers/user/cookieUser.ts
@@ -8,7 +8,7 @@ import {
 
 import {UserData} from '@interfaces/userData';
 
-export default async function getUserFromCookie(): Promise<UserData> {
+export default function getUserFromCookie(): Promise<UserData> {
     const user = {
         isAuth: false,
         username: '',
@@ -16,26 +16,29 @@ export default async function getUserFromCookie(): Promise<UserData> {
         avatar: '',
         id: -1,
     }
-    const response = await Network.user();
-    const code = response.code;
-    const data = response.data;
-    switch (code) {
-    case 200:
-        user.isAuth = true;
-        user.username = data.username;
-        user.id = data.id;
-        user.avatar = Network.getUrlFile(data.avatar);
-        user.email = data.email;
-        Events.trigger(HAVE_USER, user);
-        break;
-    case 401:
-        Events.trigger(HAVNT_USER);
-        break;
-    default:
-        Events.trigger(REDIRECT_ERROR, {url: '/error',
-            err: 'Что-то страшное произошло c нишим сервером...' + ` Он говорит: ${code}`
-        });
-        break;
-    }
-    return user;
+    const response = Network.user();
+    return response.then((r) => {
+        const code = r.code;
+        const data = r.data;
+        switch (code) {
+        case 200:
+            user.isAuth = true;
+            user.username = data.username;
+            user.id = data.id;
+            user.avatar = Network.getUrlFile(data.avatar);
+            user.email = data.email;
+            Events.trigger(HAVE_USER, user);
+            break;
+        case 401:
+            Events.trigger(HAVNT_USER);
+            break;
+        default:
+            Events.trigger(REDIRECT_ERROR, {url: '/error',
+                err: 'Что-то страшное произошло c нишим сервером...' + ` Он говорит: ${code}`
+            });
+            break;
+        }
+        return user;
+    })
+
 }
