1fd938bf087f787b5c69a0da780f7e7ef6aef76e
serenehet <serenehet@gmail.com>
2020-10-27 14:09:30 +0300

    min fix

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 
 app.use(express.static(`${__dirname}/public`));
 
-const port = 511;
+const port = 80;
 
 app.get('*', (req, res) => {
     res.sendFile(`${__dirname}/public/index.html`);
