7b9679eeea8b41aa6fbab78984a1807fb952f2f3
serenehet <serenehet@gmail.com>
2020-11-23 11:30:36 +0300

    Update constants-network.ts

--- src/helpers/network/constants-network.ts
+++ src/helpers/network/constants-network.ts
@@ -1,6 +1,6 @@
 export const BACKEND_ADDRESS_USER = 'https://hostelscan.ru:8082';
 export const BACKEND_ADDRESS_HOSTEL = 'https://hostelscan.ru:8080';
-export const BACKEND_ADDRESS_CSRF = 'https://hostelscan.ru:8080';
+export const BACKEND_ADDRESS_CSRF = 'https://hostelscan.ru:8082';
 // почему отдельно? может в ближайшем времени поменятся =)
 
 export const TEXT_ERROR_CSRF = 'Нет прав доступа';
