9890452a1099ef1ac7a0a30f0588d41aa5912d8a
Alkirys <nik_manz@mail.ru>
2020-11-25 15:50:29 +0300

    Пробуем починить бэкенд

--- src/helpers/network/constants-network.ts
+++ src/helpers/network/constants-network.ts
@@ -1,6 +1,6 @@
-export const BACKEND_ADDRESS_USER = 'https://hostelscan.ru:8082';
-export const BACKEND_ADDRESS_HOSTEL = 'https://hostelscan.ru:8080';
-export const BACKEND_ADDRESS_CSRF = 'https://hostelscan.ru:8082';
+export const BACKEND_ADDRESS_USER = 'https://hostelscan.ru:322';
+export const BACKEND_ADDRESS_HOSTEL = 'https://hostelscan.ru:228';
+export const BACKEND_ADDRESS_CSRF = 'https://hostelscan.ru:322';
 // почему отдельно? может в ближайшем времени поменятся =)
 
 export const TEXT_ERROR_CSRF = 'Нет прав доступа';
